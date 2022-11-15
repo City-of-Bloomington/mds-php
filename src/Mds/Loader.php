007838b4686640f76afb2a4836cf774f4d47395e
Cliff Ingham <inghamn@bloomington.in.gov>
2022-11-15 16:07:20 -0500

    Check for valid status_change records during load
    Updates #7

--- src/Mds/Loader.php
+++ src/Mds/Loader.php
@@ -28,15 +28,46 @@ class Loader
         }
     }
 
-    public function status_changes(string $dir)
+    /**
+     * Load a directory of JSON files into the database
+     *
+     * @param  string $dir  Directory of json files to process
+     * @return array        Invalid data from the json files
+     */
+    public function status_changes(string $dir): array
     {
-        $files = glob("$dir/*.json");
+        $files  = glob("$dir/*.json");
+        $errors = [];
         foreach ($files as $f) {
             $in   = file_get_contents($f);
             $json = json_decode($in, true);
             foreach ($json['data']['status_changes'] as $status) {
-                $this->repo->ingestStatusChange($status);
+                if (self::status_change_is_valid($status)) {
+                    $this->repo->ingestStatusChange($status);
+                }
+                else {
+                    $errors[$f][] = $status;
+                }
             }
         }
+        return $errors;
+    }
+
+    private static function status_change_is_valid(array $row): bool
+    {
+        $required = [
+            'provider_id',
+            'provider_name',
+            'device_id',
+            'vehicle_id',
+            'propulsion_type',
+            'event_type',
+            'event_type_reason',
+            'event_location'
+        ];
+        foreach ($required as $f) {
+            if (empty($row[$f])) { return false; }
+        }
+        return true;
     }
 }
